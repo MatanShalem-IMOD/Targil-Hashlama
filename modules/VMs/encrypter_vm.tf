adf6a785449dd36f42f46c912b954157a55efa8e
Matan-Shalem <matan.shalem1@gmail.com>
2024-09-16 10:14:51 +0300

    Matan started working on modules

--- modules/VMs/encrypter_vm.tf
+++ modules/VMs/encrypter_vm.tf
@@ -2,28 +2,23 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "example" {
-    name     = "example-resources"
-    location = "East US"
-}
+# resource "azurerm_virtual_network" "example" {
+#     name                = "example-network"
+#     address_space       = ["10.0.0.0/16"]
+#     location            = azurerm_resource_group.example.location
+#     resource_group_name = azurerm_resource_group.example.name
+# }
 
-resource "azurerm_virtual_network" "example" {
-    name                = "example-network"
-    address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.example.location
-    resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-    name                 = "example-subnet"
-    resource_group_name  = azurerm_resource_group.example.name
-    virtual_network_name = azurerm_virtual_network.example.name
-    address_prefixes     = ["10.0.2.0/24"]
-}
+# resource "azurerm_subnet" "example" {
+#     name                 = "example-subnet"
+#     resource_group_name  = azurerm_resource_group.example.name
+#     virtual_network_name = azurerm_virtual_network.example.name
+#     address_prefixes     = ["10.0.2.0/24"]
+# }
 
 resource "azurerm_network_interface" "example" {
     name                = "example-nic"
-    location            = azurerm_resource_group.example.location
+    location            = module.vlans.encryption_vnet_name
     resource_group_name = azurerm_resource_group.example.name
 
     ip_configuration {
--- modules/vlans/encryption_vnet.tf
+++ modules/vlans/encryption_vnet.tf
@@ -21,3 +21,15 @@ resource "azurerm_virtual_network" "create_encryption_vnet" {
   }
 }
 
+output "encryption_vnet_name" {
+  value = azurerm_virtual_network.create_encryption_vnet.name 
+}
+output "encryption_subnet_name" {
+  value = azurerm_subnet.create_encryption_vnet.subnet[0].name 
+}
+output "encryption_resource_group_name" {
+  value = azurerm_resource_group.create_encryption_resource_group.name 
+}
+output "encryption_vnet_ip" {
+  value = azurerm_virtual_network.create_encryption_vnet.subnet[0].address_prefixes[0]
+}
