25ff2e81aa96b64d880466f3351a10a87e4a1fe4
Zebi Goldstone <zebi@familygoldstone.com>
2024-09-16 15:43:03 +0300

    changed all vars to var

--- modules/vlans/cluster_vnet.tf
+++ modules/vlans/cluster_vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "create_cluster_resource_group" {
   name     = "cluster_resource_group"
-  location = vars.location
+  location = var.location
 }
 
 # create virtual network and subnet for cluster
@@ -8,24 +8,24 @@ resource "azurerm_virtual_network" "create_cluster_vnet" {
   name                = "cluster_vnet"
   location            = azurerm_resource_group.create_cluster_resource_group.location
   resource_group_name = azurerm_resource_group.create_cluster_resource_group.name
-  address_space       = vars.address_space_cluster_vnet
-  #dns_servers         = vars.dns_servers_cluster_vnet
+  address_space       = var.address_space_cluster_vnet
+  #dns_servers         = var.dns_servers_cluster_vnet
 
   subnet {
     name             = "openshift_cluster_subnet"
-    address_prefixes = vars.address_prefixes_openshift_cluster_subnet
+    address_prefixes = var.address_prefixes_openshift_cluster_subnet
   }
   subnet {
     name             = "blueteam_cluster_subnet"
-    address_prefixes = vars.address_prefixes_blueteam_cluster_subnet
+    address_prefixes = var.address_prefixes_blueteam_cluster_subnet
   }
   subnet {
     name             = "services_cluster_subnet"
-    address_prefixes = vars.address_prefixes_services_cluster_subnet
+    address_prefixes = var.address_prefixes_services_cluster_subnet
   }
   subnet {
     name             = "elk_cluster_subnet"
-    address_prefixes = vars.address_prefixes_elk_cluster_subnet
+    address_prefixes = var.address_prefixes_elk_cluster_subnet
   }
 
 
--- modules/vlans/firewall_vnet.tf
+++ modules/vlans/firewall_vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "create_firewall_resource_group" {
   name     = "firewall_resource_group"
-  location = vars.location
+  location = var.location
 }
 
 # create virtual network and subnet for firewall
@@ -8,12 +8,12 @@ resource "azurerm_virtual_network" "create_firewall_vnet" {
   name                = "firewall_vnet"
   location            = azurerm_resource_group.create_firewall_resource_group.location
   resource_group_name = azurerm_resource_group.create_firewall_resource_group.name
-  address_space       = vars.address_space_firewall_vnet
-  #dns_servers         = vars.dns_servers_firewall_vnet
+  address_space       = var.address_space_firewall_vnet
+  #dns_servers         = var.dns_servers_firewall_vnet
 
   subnet {
     name             = "main_firewall_subnet"
-    address_prefixes = vars.address_prefixes_firewall_subnet
+    address_prefixes = var.address_prefixes_firewall_subnet
   }
 
 
