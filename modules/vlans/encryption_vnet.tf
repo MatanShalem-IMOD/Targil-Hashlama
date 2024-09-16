7f25cca52cec4d79364eb159ba9aa782bc488e86
Zebi Goldstone <zebi@familygoldstone.com>
2024-09-16 15:45:49 +0300

    more vars to var

--- modules/vlans/encryption_vnet.tf
+++ modules/vlans/encryption_vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "create_encryption_resource_group" {
   name     = "encryption_resource_group"
-  location = var.location
+  location = vars.location
 }
 
 # create virtual network and subnet for encryption
@@ -8,12 +8,12 @@ resource "azurerm_virtual_network" "create_encryption_vnet" {
   name                = "encryption_vnet"
   location            = azurerm_resource_group.create_encryption_resource_group.location
   resource_group_name = azurerm_resource_group.create_encryption_resource_group.name
-  address_space       = var.address_space_encryption_vnet
-  #dns_servers         = var.dns_servers_encryption_vnet
+  address_space       = vars.address_space_encryption_vnet
+  #dns_servers         = vars.dns_servers_encryption_vnet
 
   subnet {
     name             = "main_encryption_subnet"
-    address_prefixes = var.address_prefixes_encryption_subnet
+    address_prefixes = vars.address_prefixes_encryption_subnet
   }
 
   tags = {
