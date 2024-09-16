22171915f681005b9ca16edc1ab43a71ca590182
Matan-Shalem <matan.shalem1@gmail.com>
2024-09-16 15:50:13 +0300

    Matan changed resource naming convensions

--- modules/vlans/hub_vnet.tf
+++ modules/vlans/hub_vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "create_hub_resource_group" {
   name     = "hub-resource-group"
-  location = vars.location
+  location = var.location
 }
 
 # create virtual network and subnet for hub
@@ -8,12 +8,12 @@ resource "azurerm_virtual_network" "create_hub_vnet" {
   name                = "hub-vnet"
   location            = azurerm_resource_group.create_hub_resource_group.location
   resource_group_name = azurerm_resource_group.create_hub_resource_group.name
-  address_space       = vars.address_space_hub_vnet
-  #dns_servers         = vars.dns_servers_hub_vnet
+  address_space       = var.address_space_hub_vnet
+  #dns_servers         = var.dns_servers_hub_vnet
 
   subnet {
     name             = "main-hub-subnet"
-    address_prefixes = vars.address_prefixes_main_hub_subnet
+    address_prefixes = var.address_prefixes_main_hub_subnet
   }
 
 
