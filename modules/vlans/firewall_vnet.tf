a70854c05014d0a3e0b4502796578e053cf7d093
Matan-Shalem <matan.shalem1@gmail.com>
2024-09-16 15:46:42 +0300

    Matan changed resource naming convensions

--- modules/vlans/firewall_vnet.tf
+++ modules/vlans/firewall_vnet.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "create_firewall_resource_group" {
-  name     = "firewall_resource_group"
+  name     = "firewall-resource-group"
   location = vars.location
 }
 
 # create virtual network and subnet for firewall
 resource "azurerm_virtual_network" "create_firewall_vnet" {
-  name                = "firewall_vnet"
+  name                = "firewall-vnet"
   location            = azurerm_resource_group.create_firewall_resource_group.location
   resource_group_name = azurerm_resource_group.create_firewall_resource_group.name
   address_space       = vars.address_space_firewall_vnet
   #dns_servers         = vars.dns_servers_firewall_vnet
 
   subnet {
-    name             = "main_firewall_subnet"
+    name             = "main-firewall-subnet"
     address_prefixes = vars.address_prefixes_firewall_subnet
   }
 
