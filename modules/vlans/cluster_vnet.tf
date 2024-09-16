634205be368aaf00846b38fd524882d1f874e470
Zebi Goldstone <zebi@familygoldstone.com>
2024-09-16 20:37:15 +0300

    changed format off cluster subnets

--- modules/vlans/cluster_vnet.tf
+++ modules/vlans/cluster_vnet.tf
@@ -10,27 +10,36 @@ resource "azurerm_virtual_network" "create_cluster_vnet" {
   resource_group_name = azurerm_resource_group.create_cluster_resource_group.name
   address_space       = var.address_space_cluster_vnet
   #dns_servers         = var.dns_servers_cluster_vnet
-
-  subnet {
-    name             = "openshift_cluster_subnet"
-    address_prefix = var.address_prefixes_openshift_cluster_subnet
-  }
-  subnet {
-    name             = "blueteam_cluster_subnet"
-    address_prefix = var.address_prefixes_blueteam_cluster_subnet
-  }
-  subnet {
-    name             = "services_cluster_subnet"
-    address_prefix = var.address_prefixes_services_cluster_subnet
-  }
-  subnet {
-    name             = "elk_cluster_subnet"
-    address_prefix = var.address_prefixes_elk_cluster_subnet
-  }
-
-
   tags = {
     team = "tashtiot"
   }
 }
 
+
+resource "azurerm_subnet" "create_openshift_cluster_subnet" {
+  name                 = "openshift_cluster_subnet"
+  resource_group_name  = azurerm_resource_group.create_cluster_resource_group.name
+  virtual_network_name = azurerm_virtual_network.create_cluster_vnet.name
+  address_prefixes     = var.address_prefixes_openshift_cluster_subnet # azurerm_subnet expects "address_prefixes" instead of "address_prefix"
+}
+
+resource "azurerm_subnet" "create_blue_team_cluster_subnet" {
+  name                 = "blueteam_cluster_subnet"
+  resource_group_name  = azurerm_resource_group.create_cluster_resource_group.name
+  virtual_network_name = azurerm_virtual_network.create_cluster_vnet.name
+  address_prefixes     = var.address_prefixes_blueteam_cluster_subnet
+}
+
+resource "azurerm_subnet" "create_services_cluster_subnet" {
+  name                 = "services_cluster_subnet"
+  resource_group_name  = azurerm_resource_group.create_cluster_resource_group.name
+  virtual_network_name = azurerm_virtual_network.create_cluster_vnet.name
+  address_prefixes     = var.address_prefixes_services_cluster_subnet
+}
+
+resource "azurerm_subnet" "create_elk_cluster_subnet" {
+  name                 = "elk_cluster_subnet"
+  resource_group_name  = azurerm_resource_group.create_cluster_resource_group.name
+  virtual_network_name = azurerm_virtual_network.create_cluster_vnet.name
+  address_prefixes     = var.address_prefixes_elk_cluster_subnet
+}
