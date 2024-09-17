d4f05cd7d8fae045ca621a0623b986017c66db7a
ItayZarkon <itayzarkon@gmail.com>
2024-09-17 21:22:24 +0300

    add reverce peering to all

--- modules/peering/main.tf
+++ modules/peering/main.tf
@@ -40,6 +40,23 @@ resource "azurerm_virtual_network_peering" "create_peer_hub_to_firewall" {
   allow_gateway_transit = false
 }
 
+#########reverse peering################
+resource "azurerm_virtual_network_peering" "create_peer_firewall_to_hub" {
+  name                      = "firewall-to-hub"
+  resource_group_name       = var.firewall_resource_group_name
+  virtual_network_name      = var.firewall_vnet_name
+  remote_virtual_network_id = var.hub_vnet_id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+
+  # `allow_gateway_transit` must be set to false for vnet Global Peering
+    allow_gateway_transit = false
+}
+
+#######################################
+
+
 resource "azurerm_virtual_network_peering" "create_peer_firewall_to_cluster" {
   name                      = "firewall-to-cluster"
   resource_group_name       = var.firewall_resource_group_name
@@ -52,6 +69,22 @@ resource "azurerm_virtual_network_peering" "create_peer_firewall_to_cluster" {
   # `allow_gateway_transit` must be set to false for vnet Global Peering
   allow_gateway_transit = false
 }
+
+#########reverse peering################
+resource "azurerm_virtual_network_peering" "create_peer_cluster_to_firewall" {
+  name                      = "cluster-to-firewall"
+  resource_group_name       = var.cluster_resource_group_name
+  virtual_network_name      = var.cluster_vnet_name
+  remote_virtual_network_id = var.firewall_vnet_id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+
+  # `allow_gateway_transit` must be set to false for vnet Global Peering
+    allow_gateway_transit = false
+}
+
+#######################################
 
 # resource "azurerm_virtual_network_peering" "create_peer_vpn_to_cluster" {
 #   name                      = "cluster-to-vpn"
