a416ab320c324c84a5b8b691ed59223c3a3e2362
Matan-Shalem <matan.shalem1@gmail.com>
2024-09-18 13:59:12 +0300

    added new rg

--- modules/Firewall/firewall.tf
+++ modules/Firewall/firewall.tf
@@ -1,14 +1,14 @@
-# Create Firewall Public IP
-resource "azurerm_public_ip" "firewall_pip" {
-    name                = var.firewall_public_ip_name
-    location            = var.firewall_vnet_location
-    resource_group_name = var.firewall_vnet_resource_group_name
-    allocation_method   = "Static"
-    sku                 = "Standard"
-    tags = {
-    owned_by = "Hashlama015"
-    }
-}
+# # Create Firewall Public IP
+# resource "azurerm_public_ip" "firewall_pip" {
+#     name                = var.firewall_public_ip_name
+#     location            = var.firewall_vnet_location
+#     resource_group_name = var.firewall_vnet_resource_group_name
+#     allocation_method   = "Static"
+#     sku                 = "Standard"
+#     tags = {
+#     owned_by = "Hashlama015"
+#     }
+# }
 
 # Create Firewall Policy
 resource "azurerm_firewall_policy" "firewall_policy" {
@@ -25,17 +25,17 @@ resource "azurerm_firewall_policy_rule_collection_group" "firewall_policy_rule_c
     name                = "BasicRuleCollectionGroups"
     firewall_policy_id = azurerm_firewall_policy.firewall_policy.id
     priority = 300
-    network_rule_collections {
+    network_rule_collection {
         name = "networkRuleCollection1"
         priority = 300
         action = "Allow"
         rule {
             name = "Allow-Any-Any"
-            rule_type = "NetworkRule"
+            #rule_type = "NetworkRule"
             source_addresses = ["*"]
             destination_addresses = ["*"]
             destination_ports = ["*"]
-            protocols = ["*"]    
+            protocols = ["Any"]    
         }
     }
     
@@ -51,9 +51,9 @@ resource "azurerm_firewall" "azure_firewall" {
     firewall_policy_id = azurerm_firewall_policy.firewall_policy.id
 
     ip_configuration {
-        name                 = "configuration"
+        name                 = "internal-configuration"
         subnet_id            = var.firewall_vnet_subnet_id
-        public_ip_address_id = azurerm_public_ip.firewall_pip.id
+        # public_ip_address_id = azurerm_public_ip.firewall_pip.id
     }
     tags = {
     owned_by = "Hashlama015"
